21251c5341f7c1b75f3a9f6a022cd4a1902fd720
robbusblobbus <robbie.aitken@hotmail.co.uk>
2022-03-08 17:16:53 +0000

    Completed module

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -5,6 +5,7 @@ import com.pluralsight.blog.data.PostRepository;
 import com.pluralsight.blog.model.Category;
 import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,15 +17,19 @@ import java.util.Optional;
 public class BlogController {
 
     private PostRepository postRepository;
+    private CategoryRepository categoryRepository;
 
-    public BlogController(PostRepository postRepository) {
+    public BlogController(PostRepository postRepository, CategoryRepository categoryRepository) {
         this.postRepository = postRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {
         List<Post> posts = postRepository.findAll();
         modelMap.put("posts", posts);
+        List<Category> categories = categoryRepository.findAll();
+        modelMap.put("categories", categories);
         return "home";
     }
 
@@ -33,5 +38,19 @@ public class BlogController {
         Post post = postRepository.findById(id).orElse(null);
         modelMap.put("post", post);
         return "post-details";
+    }
+
+    @RequestMapping("/category/{id}")
+    public String categoryList(@PathVariable Long id, ModelMap modelMap) {
+        Category category = categoryRepository.findById(id).orElse(null);
+        modelMap.put("category", category);
+
+        List<Post> posts = postRepository.findByCategory(category);
+        modelMap.put("posts", posts);
+
+        List<Category> categories = categoryRepository.findAll();
+        modelMap.put("categories", categories);
+
+        return "category-list";
     }
 }
